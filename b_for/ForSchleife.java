0367e52279db9b5a204b6b8b786f2ba1338b776e
Maximiliano Arenas <max.arenas@gmail.com>
2025-10-07 12:04:17 +0200

    Add comments to the ForSchleife class

--- b_for/ForSchleife.java
+++ b_for/ForSchleife.java
@@ -4,6 +4,16 @@ public class ForSchleife
    {
          // Wann ; Bedingungen ; "Formula" i++ = i=i+1
          // Buscar composición de for()
+         // Der Kopf der for-Schleife eteuert die
+         // Anzahl der Wiederholungen.
+
+         // Er hat drei Abschnitte:
+         //
+         // 1.) Deklaration der Zaehlvariablen i
+         // und Initialisierung mit den STARTWERT 1.
+         // 2.) Bedingung fuer den ENDWERT: i darf nur
+         // kleiner oder gleich nach 20 sein (nicht groesser)
+
       for(int i = 1;i <= 20; i++)
       {
          System.out.println("Hallo " + i);
