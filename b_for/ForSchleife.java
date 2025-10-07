fdd0a2e91581bc51986993bd594033a3967c34dc
Maximiliano Arenas <max.arenas@gmail.com>
2025-10-07 11:25:07 +0200

    Create FOR Command in the ForSchleife file

---
+++ b_for/ForSchleife.java
@@ -0,0 +1,15 @@
+public class ForSchleife
+{
+   public static void main(String[] args)
+   {
+         // Wann ; Bedingungen ; "Formula" i++ = i=i+1
+         // Buscar composición de for()
+      for(int i = 1;i <= 20; i++)
+      {
+         System.out.println("Hallo " + i);
+      }
+   
+      
+   }
+   
+}
